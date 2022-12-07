d6fd4e40cf84e977ebda7bf318153b383432d533
Misaki1505 <sawako1505@gmail.com>
2022-12-06 21:09:54 -0500

    06/12/2022 Changes

--- app/src/main/java/com/example/hangman/Hangman.kt
+++ app/src/main/java/com/example/hangman/Hangman.kt
@@ -1,5 +1,19 @@
 package com.example.hangman
 
+import kotlin.random.Random
+
+val words = listOf("february", "kotlin","star", "translate", "athletic", "cowboy", "arcane", "slayer", "predictable", "watching")
+var word = ""
+val guesses = arrayListOf<Char>()
+var remainigGuesses = 6
+var mistakes = 0
+
 fun main(args: Array<String>) {
 
+}
+
+fun setupGame() {
+    val wordIndex = Random.nextInt(words.size)
+    word = words[wordIndex]
+    println(word)
 }
